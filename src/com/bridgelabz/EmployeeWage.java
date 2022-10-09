9c8de6478ed3dca9ad4f32403dd47f091fccab88
shipramargurate <shivanikumari22322@gmail.com>
2022-10-09 16:32:48 +0530

    [shivani] : Added : functionality for employee ages for multiples company

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class EmployeeWage {
 	/*
-	 *  Refactor the Code to write a Class Method to Compute Employee Wage
+	 * Compute Employee Wage for multiple companies
 	 */
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HOURS_IN_MONTH = 100;
+//	public static final int EMP_RATE_PER_HOUR = 20;
+//	public static final int NUM_OF_WORKING_DAYS = 20;
+//	public static final int MAX_HOURS_IN_MONTH = 100;
 
-	public int wage() {
+	public int wage(String company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth) {
 		int empHour = 0;
 		int totalEmpHours = 0;
 		int totalWorkingDays = 0;
 
-		while (totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) { // condition till working hours reaches for month
+		while (totalEmpHours <= maxHourPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 
-			Random random = new Random(); //random object
-			int attendance = random.nextInt(3); // 0,1,2
+			Random random = new Random();
+			int attendance = random.nextInt(3);
 			switch (attendance) {
 			case IS_PART_TIME:
 				System.out.println("Part Time");
@@ -42,9 +42,10 @@ public class EmployeeWage {
 			System.out.println("Day " + totalWorkingDays + " Emp Hour: " + empHour);
 
 		}
-		int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+		int totalEmpWage = totalEmpHours * empRatePerHour;
 		System.out.println("Total Emp Wage : " + totalEmpWage);
 		return totalEmpWage;
 
 	}
+
 }
