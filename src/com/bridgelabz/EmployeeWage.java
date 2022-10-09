d259e60f7a35a760507b907a216fee9cb83ed219
shipramargurate <shivanikumari22322@gmail.com>
2022-10-09 15:18:10 +0530

    [Shivani]: welcome to Employee wage computation program

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Employee Wage computation program");
+
+	}
+
+}
