1b5b882b9a811ae28e5d535262bca1bd1a221497
shipramargurate <shivanikumari22322@gmail.com>
2022-10-09 15:28:08 +0530

    [SHIVANI]: Added calculation of daily employee wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,20 +4,24 @@ import java.util.*;
 
 public class EmployeeWage {
 	/*
-	 * Check Employee is Present or Absent using random
+	 * Calculate Daily Employee Wage assuming wage per hour is 20 and full day hour is 8
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcomr to Employee Wage Computation Program\n");
 
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHour = 0;
+		int empWage = 0;
 		Random random = new Random(); // random object
 		int attendance = random.nextInt(2); // will give 0 or 1 random number
 
-		if (attendance == IS_FULL_TIME) // condition for present or absent
-			System.out.println("Employee is Present");
+		if (attendance == IS_FULL_TIME)
+			empHour = 8;
 		else
-			System.out.println("Employee is Absent");
-
+			empHour = 0;
+		empWage = empHour * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage : " + empWage);
 	}
 
 }
